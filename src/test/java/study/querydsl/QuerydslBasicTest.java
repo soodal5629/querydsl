16070b091ffba9fbf9c8c9ba288cf46826371a58
chaelynjang <chaelynjang@lotte.net>
2022-11-19 12:42:48 +0900

    결과 조회 테스트

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -1,5 +1,6 @@
 package study.querydsl;
 
+import com.querydsl.core.QueryResults;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,6 +13,8 @@ import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static study.querydsl.entity.QMember.member; // static import
@@ -80,4 +83,22 @@ public class QuerydslBasicTest {
                 .fetchOne();
         assertThat(findMember.getUsername()).isEqualTo("member1");
     }
+
+    @Test
+    public void resultFetch(){
+        List<Member> fetch = queryFactory.selectFrom(member)
+                .fetch();
+        Member fetchOne = queryFactory.selectFrom(member)
+                .fetchOne();
+        Member fetchFirst = queryFactory.selectFrom(member)
+                .fetchFirst();
+        QueryResults<Member> results = queryFactory.selectFrom(member)
+                .fetchResults();
+        //results.getTotal();
+        List<Member> content = results.getResults(); // 컨텐트(데이터)
+
+        long total = queryFactory.selectFrom(member)
+                .fetchCount();
+    }
+
 }
