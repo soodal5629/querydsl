1ac53cc3def89aa9b312e863c2937f7f928a7fe1
chaelynjang <chaelynjang@lotte.net>
2022-11-20 20:49:38 +0900

    기본 조인 예제 실습

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -195,4 +195,34 @@ public class QuerydslBasicTest {
         assertThat(teamB.get(member.age.avg())).isEqualTo(35);
     }
 
+    @Test
+    public void join() {
+        List<Member> result = queryFactory.selectFrom(member)
+                .join(member.team, team) // inner join과 동일
+                .where(team.name.eq("teamA"))
+                .fetch();
+
+        assertThat(result)
+                .extracting("username")
+                .containsExactly("member1", "member2");
+    }
+
+    /*
+     * 세타 조인(연관 관계가 없는 조인)도 가능
+     * 회원이 이름이 팀 이름과 같은 회원 조회
+     */
+    @Test
+    public void theta_join() {
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+
+        List<Member> result = queryFactory.select(member)
+                .from(member, team)
+                .where(member.username.eq(team.name))
+                .fetch(); // 모든 회원과 모든 팀을 다 가져와서 조인해버린 후, where 절에서 필터링
+
+        assertThat(result)
+                .extracting("username")
+                .containsExactly("teamA", "teamB");
+    }
 }
