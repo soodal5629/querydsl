93f3dcb846d0031d051e42a54f4416de2606e14c
chaelynjang <chaelynjang@lotte.net>
2022-11-29 21:35:30 +0900

    @QueryProjection 이용한 dto 결과 반환 예제 실습

--- src/main/java/study/querydsl/dto/MemberDto.java
+++ src/main/java/study/querydsl/dto/MemberDto.java
@@ -1,5 +1,6 @@
 package study.querydsl.dto;
 
+import com.querydsl.core.annotations.QueryProjection;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,6 +11,8 @@ public class MemberDto {
     private String username;
     private int age;
 
+    @QueryProjection // 이거 추가 후, gradle => compileQuerydsl 누르면 dto도 Q 파일로 생성됨
+    // 편리하긴 하나, 해당 DTO가 Querydsl에 대한 의존성을 갖게 되어 버림(단점,,)
     public MemberDto(String username, int age) {
         this.username = username;
         this.age = age;
